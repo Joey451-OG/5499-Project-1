3b78e604d21ec15c1a6d46f4baf208aaa3403553
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-14 11:59:20 -0500

    Basic movement down | Untested

---
+++ Scripts/gdscript/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+@export var VELCOCITY_IMPULSE = 300.0
+@export var DRAG = 1.1
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+func _process(delta: float) -> void:
+	sprite.flip_v = !is_in_range(rad_to_deg(rotation), -90, 90)
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	look_at(get_global_mouse_position())
+	velocity = transform.x * Input.get_action_strength("swim") * VELCOCITY_IMPULSE
+	
+	# sinking
+	#if not Input.is_action_pressed("swim"):
+		#velocity.y += VELCOCITY_IMPULSE * 0.5
+	#
+
+	move_and_slide()
+
+# Checks if value is in range (min, max) (exclusive)
+func is_in_range(value: float, min: float, max: float) -> bool:
+	#print("Value: %.2f: %s" % [value, min < value and value < max])
+	return min < value and value < max
