1e7d3b7231b0ab8c8410ed522a3960c11dea6f74
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-14 15:41:12 -0500

    BF & F | Fixed sprite flipping and added drowning
    I now scale the entire sprite object to -1 when I want to flip it.
    
    Drowning works by starting a timer if you're underwater.

--- Scripts/gdscript/player.gd
+++ Scripts/gdscript/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody2D
 
 
-@export var VELCOCITY_IMPULSE = 300.0
-@export var DRAG = 1.1
+@export var VELCOCITY_IMPULSE := 300.0
+@export var DRAG := 1.1
+@export var lung_capacity_in_seconds := 10
+
+var isUnderWater := true
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var lung_timer: Timer = $lungTimer
+@onready var lung_indicator: Line2D = $Sprite2D/lungIndicator
 
 func _process(delta: float) -> void:
-	sprite.flip_v = !is_in_range(rad_to_deg(rotation), -90, 90)
+	sprite.scale = Vector2(1, 1) if is_in_range(rad_to_deg(rotation), -90, 90) else Vector2(-1, -1)
+	
+	if lung_timer.is_stopped() and isUnderWater:
+		lung_timer.start(lung_capacity_in_seconds)
+	
+	lung_indicator.scale.x = 1 - (( lung_timer.wait_time - lung_timer.time_left ) / lung_timer.wait_time)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,3 +39,7 @@ func _physics_process(delta: float) -> void:
 func is_in_range(value: float, min: float, max: float) -> bool:
 	#print("Value: %.2f: %s" % [value, min < value and value < max])
 	return min < value and value < max
+
+
+func _on_lung_timer_timeout() -> void:
+	print("[LUNG TIMER]: Player Drowned!")
