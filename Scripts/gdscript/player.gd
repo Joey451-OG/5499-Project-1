7b76de67abdf3902f91fe9abf07f3f0f7bcd991e
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-15 13:22:28 -0500

    Playtesting | Removed Sinking
    After playtesting, I found that the sinking mechanic wasn't fun.
    
    Also added beaver sprite in this push

--- Scripts/gdscript/player.gd
+++ Scripts/gdscript/player.gd
@@ -1,21 +1,26 @@
 extends CharacterBody2D
 
-
 @export var VELCOCITY_IMPULSE := 300.0
 @export var DRAG := 1.1
 @export var lung_capacity_in_seconds := 10
 
-var isUnderWater := true
+var isUnderWater := false
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var lung_timer: Timer = $lungTimer
 @onready var lung_indicator: Line2D = $Sprite2D/lungIndicator
 
 func _process(delta: float) -> void:
-	sprite.scale = Vector2(1, 1) if is_in_range(rad_to_deg(rotation), -90, 90) else Vector2(-1, -1)
+	if is_in_range(rad_to_deg(rotation), -90, 90):
+		sprite.scale = abs(sprite.scale)
+	elif sprite.scale.y > 0:
+		sprite.scale.y = -sprite.scale.y
 	
 	if lung_timer.is_stopped() and isUnderWater:
 		lung_timer.start(lung_capacity_in_seconds)
+	
+	if not isUnderWater and !lung_timer.is_stopped():
+		lung_timer.stop()
 	
 	lung_indicator.scale.x = 1 - (( lung_timer.wait_time - lung_timer.time_left ) / lung_timer.wait_time)
 
@@ -29,8 +34,8 @@ func _physics_process(delta: float) -> void:
 	velocity = transform.x * Input.get_action_strength("swim") * VELCOCITY_IMPULSE
 	
 	#sinking
-	if not Input.is_action_pressed("swim"):
-		velocity.y += VELCOCITY_IMPULSE * 0.5
+	#if not Input.is_action_pressed("swim"):
+		#velocity.y += VELCOCITY_IMPULSE * 0.5
 		
 
 	move_and_slide()
@@ -40,6 +45,10 @@ func is_in_range(value: float, min: float, max: float) -> bool:
 	#print("Value: %.2f: %s" % [value, min < value and value < max])
 	return min < value and value < max
 
-
 func _on_lung_timer_timeout() -> void:
 	print("[LUNG TIMER]: Player Drowned!")
+
+func _on_interacting_hit_box_area_entered(area: Area2D) -> void:
+	if area.has_meta("isAir"):
+		isUnderWater = !area.get_meta("isAir")
+		print(isUnderWater)
