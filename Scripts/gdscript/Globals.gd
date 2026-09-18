667360ad98473e6df0603200a12aadb4a5160b8d
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-18 16:56:38 -0500

    More progress on Upgrade Station

--- Scripts/gdscript/Globals.gd
+++ Scripts/gdscript/Globals.gd
@@ -1,8 +1,10 @@
 extends Node
 
-
-enum PowerUps {MoreLungCapacity, FasterSwimming}
 enum PlayerState {IDLE, SWIMING, DROWNED}
 
 var points := 0
 var p_state : PlayerState = PlayerState.IDLE
+var current_abilities := {
+	"speed" : false,
+	"higher_lc" : false, # Higher lung capacity
+}
--- Scripts/gdscript/upgrade_station.gd
+++ Scripts/gdscript/upgrade_station.gd
@@ -1,5 +1,8 @@
 extends Sprite2D
-
+var ability_unlock_sticks = {
+	"speed": 4,
+	"higher_lc": 10000
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event.is_action_pressed("swim"):
+		for key in ability_unlock_sticks.keys():
+			if Globals.points > ability_unlock_sticks[key] and !Globals.current_abilities[key]:
+				pass
