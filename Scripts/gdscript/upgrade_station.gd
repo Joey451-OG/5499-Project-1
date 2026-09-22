0ee46584b2bf0a37c70db037339165e440d3ff17
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-22 11:04:38 -0500

    Playtest | Changed US interaction method
    Changed US interaction from clicking to simply swiming over it. In
    playtesting, all players tried to swim/move over the US instead of
    clicking it.

--- Scripts/gdscript/upgrade_station.gd
+++ Scripts/gdscript/upgrade_station.gd
@@ -17,8 +17,8 @@ func start_indicator():
 			animated_sprite_2d.play("indicating")
 			continue
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event.is_action_pressed("swim"):
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if Input.is_action_pressed("swim"):
 		for key in ability_unlock_sticks.keys():
 			if Globals.points >= ability_unlock_sticks[key] and !Globals.current_abilities[key]:
 				print("[update_station.gd]: Unlocked %s!" % key)
