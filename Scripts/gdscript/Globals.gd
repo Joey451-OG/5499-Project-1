8be0c00e2b98782c78e5329f398c604315d2ec99
Joseph "Joey" Garneau <itsjoeygofficial@outlook.com>
2026-09-23 09:27:42 -0500

    Playtest | Buffed HLC and Speed on Breaker upgrade
    Also added screen 4.
    
    Screen 4 was impossible to complete without these buffs. Buffs work by
    adding 0.2 to all ability multiplers whenever the game detects the
    breaker upgrade was aquired.

--- Scripts/gdscript/Globals.gd
+++ Scripts/gdscript/Globals.gd
@@ -5,6 +5,8 @@ signal pointsUntilUpdate
 
 enum PlayerState {IDLE, SWIMING, DROWNED}
 
+@export var ramped_ability_increase_percentage := 0.2
+
 var points := 0 :
 	set(value):
 		points = value
@@ -15,9 +17,10 @@ var points_until_next_upgrade : int = 5:
 		points_until_next_upgrade = value
 		emit_signal("pointsUntilUpdate")
 
-var god_mode : bool = true
+var god_mode : bool = false
 var vignette_scale : float = 0.0
 var p_state : PlayerState = PlayerState.IDLE
+var added_upgrade_lock := false
 
 var current_abilities := {
 	"higher_lc" : false, # Higher lung capacity
@@ -34,4 +37,13 @@ func _ready() -> void:
 	if god_mode:
 		for key in current_abilities.keys():
 			current_abilities[key] = true
+
+func _process(delta: float) -> void:
+	_check_for_ramped_abilites()
+	
+func _check_for_ramped_abilites():
+	if current_abilities["breaker"] and not added_upgrade_lock:
+		added_upgrade_lock = true
+		for key in ability_modifiers.keys():
+			ability_modifiers[key] += ramped_ability_increase_percentage
 	
